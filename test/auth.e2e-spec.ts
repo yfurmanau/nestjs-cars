ee4dd7398682c2d59f1e09cd5d47f115aeb3bb27
yfurmanau <yfurmanau@gmail.com>
2024-06-05 22:52:30 +0200

    add signin e2e test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -25,4 +25,20 @@ describe('Authentication System', () => {
     expect(id).toBeDefined();
     expect(email).toBe(targetEmail);
   });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'test3@mail.io';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: email, password: 'pass123' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
+  });
 });
